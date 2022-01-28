7708a21558c98b1c1a1528ff0a2f19452044174d
TungNguyen2000 <49642102+TungNguyen2000@users.noreply.github.com>
2022-01-29 00:41:20 +0700

    Add .Net API

---
+++ doan_ptud_httt/doan_ptud_httt_api/Controllers/OrdersController.cs
@@ -0,0 +1,61 @@
+﻿using doan_ptud_httt_api.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace doan_ptud_httt_api.Controllers
+{
+    [Microsoft.AspNetCore.Mvc.Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private datainstance db = new datainstance();
+
+        // GET: api/<OrdersController>
+        [Microsoft.AspNetCore.Mvc.HttpGet]
+        public IQueryable<Order> Get()
+        {
+            return db.order;
+        }
+
+        // GET api/<OrdersController>/5
+        [Microsoft.AspNetCore.Mvc.HttpGet("{id}")]
+        public async Task<IHttpActionResult> Get(int id)
+        {
+            Order order = await db.order.FindAsync(id);
+            if (order == null)
+                return (IHttpActionResult)NotFound();
+
+            return (IHttpActionResult)Ok(order);
+        }
+
+        // POST api/<OrdersController>
+        [Microsoft.AspNetCore.Mvc.HttpPost]
+        public void Post([Microsoft.AspNetCore.Mvc.FromBody] string value)
+        {
+        }
+
+        // PUT api/<OrdersController>/5
+        [Microsoft.AspNetCore.Mvc.HttpPut("{id}")]
+        public void Put(int id, [Microsoft.AspNetCore.Mvc.FromBody] string value)
+        {
+        }
+
+        // DELETE api/<OrdersController>/5
+        [Microsoft.AspNetCore.Mvc.HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ doan_ptud_httt/doan_ptud_httt_api/Model/Order.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Data;
+using System.Data.Entity;
+
+namespace doan_ptud_httt_api.Model
+{
+    public class Order
+    {
+        private int orderID { get; set; }
+        private int orderQuantity { get; set; }
+        private int orderprice { get; set; }
+        private DateTime date { get; set; }
+        private String address { get; set; }
+        private String phone { get; set; }
+        private int buyerid { get; set; }
+        private int shipperid { get; set; }
+    }
+
+    public class datainstance:DbContext
+    {
+        public DbSet<Order> order { get; set; }
+    }
+}
